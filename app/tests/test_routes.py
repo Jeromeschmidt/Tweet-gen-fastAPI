f9a25d925c3611f7c370818aca674d3979d8c230
Jerome <jerome.schmidt@students.makeschool.com>
2021-08-12 14:55:33 -0400

    added route tests

---
+++ app/tests/test_routes.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from datetime import datetime
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
+sys.path.insert(0, PROJECT_DIR)
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from app.core.main import app
+
+client = TestClient(app)
+
+
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
+def test_generate_tweet():
+    response = client.get("/tweets")
+    assert response.status_code == 200
+    assert type(response.json()["tweet"]) is str
+
+def test_create_tweet():
+    response = client.post("/tweets/test300")
+    assert response.status_code == 200
+    assert response.json()['data'][0]["tweet"] == "test300"
+    assert response.json()["message"] == "Tweet added successfully."
+
+def test_get_all_tweets():
+    response = client.get("/tweets/all")
+    assert response.status_code == 200
+    assert type(response.json()["tweets"]) is list
+
+def test_delete_tweet():
+    # Add Tweet and Get tweet id to test delete
+    response = client.post("/tweets/test300")
+    assert response.status_code == 200
+    assert response.json()['data'][0]["tweet"] == "test300"
+    assert response.json()["message"] == "Tweet added successfully."
+    id = response.json()['data'][0]["_id"]
+
+    # Test Delete Tweet that was just added to db
+    response = client.delete("/tweets/" + id)
+    assert response.status_code == 200
+    assert response.json()["data"] == ['Tweet with ID: ' + id + ' removed']
+    assert response.json()["message"] == 'Tweet deleted successfully'
+
+    # Test Delete Tweet that was just deleted from db
+    assert response.status_code == 200
+    assert response.json()["error"] == 'An error occurred'
+    assert response.json()["code"] == 404
+    assert response.json()["message"] == "Tweet with id " + id + " doesn't exist"
